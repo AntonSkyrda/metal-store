dbd370011d1c5a2cb4629126f4e60ccace3eadfe
AntonSkyrda <mr.skyrda@gmail.com>
2024-07-19 08:10:18 +0300

    create views for goods

--- store/views.py
+++ store/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import (
+    Category,
+    SubCategory,
+    MetalCircle,
+    MetalCircleSizes,
+    MetalSheet,
+    MetalSheetsSizes,
+    MetalPipe,
+    MetalPipeSizes,
+)
+
+
+def index(request):
+    return render(request, "store/index.html")
+
+
+class MetalCircleListView(generic.ListView):
+    model = MetalCircle
+    paginate_by = 10
+
+
+class MetalCircleDetailView(generic.DetailView):
+    model = MetalCircle
+
+
+class MetalSheetListView(generic.ListView):
+    model = MetalSheet
+    paginate_by = 10
+
+
+class MetalSheetDetailView(generic.DetailView):
+    model = MetalSheet
+
+
+class MetalPipeListView(generic.ListView):
+    model = MetalPipe
+    paginate_by = 10
+
+
+class MetalPipeDetailView(generic.DetailView):
+    model = MetalPipe
