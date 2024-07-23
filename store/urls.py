8611d6e9230b076c4b0f1abab457b19f85ae5553
AntonSkyrda <mr.skyrda@gmail.com>
2024-07-24 00:47:49 +0300

    update urls.py

--- store/urls.py
+++ store/urls.py
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("metal-circles/", MetalCircleListView.as_view(), name="metal-circle-list"),
-    path("metal-circles/<int:pk>", MetalCircleDetailView.as_view(), name="metal-circle-details")
+    path("metal-circles/<int:pk>/", MetalCircleDetailView.as_view(), name="metal-circle-details")
 ]
 
 app_name = "store"
