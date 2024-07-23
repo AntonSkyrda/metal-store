6b20240fd3daa41e99f5b6b0df062edd0b168074
AntonSkyrda <mr.skyrda@gmail.com>
2024-07-24 00:31:45 +0300

    fix static and templates

--- metal_store/urls.py
+++ metal_store/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls', namespace='store'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, )
